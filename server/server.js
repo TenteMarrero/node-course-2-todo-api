9c9d8260107a17084019e49410f0e3ce7d1c726c
QTSEL\VMA <vicente.marrero@qlik.com>
2017-09-28 14:52:50 +0100

    Mongoose test v2.0

--- server/server.js
+++ server/server.js
@@ -5,12 +5,56 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
+});
+
+// var newTodo = new Todo({
+//     text: 'Cook dinner'
+// });
+
+// newTodo.save().then((doc) => {
+//     console.log('Save todo', doc);
+// }, (e) => {
+//     console.log('Unable to save todo');
+// });
+
+// var otherTodo = new Todo({
+//     text: 'Something to do'
+// });
+
+// otherTodo.save().then((doc) => {
+//     console.log('Save todo', doc);
+// }, (e) => {
+//     console.log('Unable to save todo', e);
+// });
+
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }
+});
+
+var newUser = new User({
+    email: ' tente.marrero@gmail.com'
+});
+
+newUser.save().then((doc) => {
+    console.log('Save User', doc);
+}, (e) => {
+    console.log('Unable to save user', e);
 });
