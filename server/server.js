acf9c2ae6a9a620ef5e57645ab19a6a93d4d1d86
QTSEL\VMA <vicente.marrero@qlik.com>
2017-09-29 18:37:21 +0100

    Add DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -45,6 +45,21 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.sendStatus(404);
+    }
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }, (e) => {
+        res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
